7679cb09c17b29a01eea1a7097d1d641aaa7292a
Natalia Lopez <natigab.24@gmail.com>
2024-01-12 01:16:38 -0300

    agregamos el componente Footer y corregí el maquetado del componente FormularioTareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,14 +1,25 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form } from "react-bootstrap";
 //import Form from 'react-bootstrap/Form';
-import ListaTareas from './ListaTareas';
+import ListaTareas from "./ListaTareas";
 const FormularioTareas = () => {
   return (
     <section>
       <Form>
-        <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
-          <Form.Control type="text" placeholder="ej: tarea 1" minLength={3} maxLength={50} />
+        <Form.Group
+          className="mb-3 d-flex"
+          controlId="exampleForm.ControlInput1"
+        >
+          <Form.Control
+            type="text"
+            placeholder="ej: tarea 1"
+            minLength={3}
+            maxLength={50}
+          />
+
+          <Button variant="info" className="mx-2" type="submit">
+            Enviar
+          </Button>
         </Form.Group>
-        <Button variant='info' className='mx-2' type='submit'>Enviar</Button>
       </Form>
       <ListaTareas></ListaTareas>
     </section>
