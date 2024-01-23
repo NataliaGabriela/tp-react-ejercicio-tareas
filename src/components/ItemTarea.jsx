23289d7bfbce0604c6882ab02ea8c402a577339d
Natalia Lopez <natigab.24@gmail.com>
2024-01-22 23:45:06 -0300

    agregamos la lógica del boton borrar

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -4,7 +4,7 @@ import { ListGroup } from 'react-bootstrap';
 const ItemTarea = ({nombreTarea}) => {
     return (
         <ListGroup.Item className='d-flex justify-content-between'>{nombreTarea}
-        <Button variant="danger">Borrar</Button></ListGroup.Item>
+        <Button variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button></ListGroup.Item>
     );
 };
 
